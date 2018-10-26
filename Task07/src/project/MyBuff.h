87163927cfc25fe20c1dbf8b1dd3ba0dc439084c
tailerr <alexa-1903@rambler.ru>
2018-10-26 09:12:44 +0200

    Task07

--- Task07/src/project/MyBuff.h
+++ Task07/src/project/MyBuff.h
@@ -7,25 +7,23 @@ class LineNumberBuffer : public std::streambuf
     std::streambuf* old;
     std::string str;
     std::string res;
+    bool f=true;
     int N = 0;
     public:
         explicit LineNumberBuffer(std::streambuf* b): old(b) {}
     protected:
         int_type overflow(int_type c) override {
-            if (N == 0){
+            if (f){
                 str = std::to_string(N)+" ";
                 old->sputn(str.data(), str.size());
+                f = false;
                 N++;
             }
-            if (c == '\n'){
-                auto x = old->sputc(c);
-                str = std::to_string(N)+" ";
-                old->sputn(str.data(), str.size());
-                N++;
-                return x + str.size();
-            }
+            if (c == '\n') 
+                f = true;
             return old->sputc(c);    
         }
+ 
         std::streamsize xsputn(const char* s, std::streamsize n) override {
             str.assign(s, n);
             if (N == 0){
@@ -33,11 +31,11 @@ class LineNumberBuffer : public std::streambuf
                 N++;
             }
             else
-                res = "";    
+                res = "";        
             for (char& ch : str){
                 res += ch;
                 if (ch == '\n'){
-                    res += std::to_string(N);
+                    res += std::to_string(N)+" ";
                     res += " ";
                     N++;
                 }
--- Task07/src/project/main.cc
+++ Task07/src/project/main.cc
@@ -4,10 +4,12 @@
 
 int main(){
     
+    std::streambuf* oldbuf = std::cout.rdbuf();
     LineNumberBuffer b(std::cout.rdbuf());
 
     std::cout.rdbuf(&b);
     std::cout << std::cin.rdbuf();
+    std::cout.rdbuf(oldbuf);
 
     return 0;
 }
