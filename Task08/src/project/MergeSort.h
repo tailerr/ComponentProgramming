b149ca2fa363c099e27daa1ecde65c00c68e6cd4
tailerr <alexa-1903@rambler.ru>
2018-11-04 15:19:05 +0200

    Task08

---
+++ Task08/src/project/MergeSort.h
@@ -0,0 +1,104 @@
+#include<iterator>
+#include<vector>
+#include <future>
+#include <thread>
+#include<functional>
+
+
+template <typename T>
+bool my_compare(T lhs, T rhs)
+{
+    return *rhs > *lhs;
+}
+
+template<typename T>
+void merge(int dist, const T begin, T middle, const T end, bool(*compare)(T, T)){    
+    T l = begin;
+    T r = middle;
+    
+    std::vector<typename T::value_type> tmp(dist);
+    auto k = tmp.begin();
+    while(l!=middle && r!=end){
+        if(compare(l, r)){
+            copy(l, l+1, k);
+            ++k;
+            ++l;
+        }
+        else{
+            copy(r, r+1, k);
+            ++k;
+            ++r;
+        }
+    }
+    while(l!=middle){
+        copy(l, l+1, k);
+        ++k;
+        ++l;
+    }   
+    while(r!=end){
+        copy(r, r+1, k);
+        ++k;
+        ++r;
+    }
+    auto b = tmp.begin();
+    auto e = tmp.end(); 
+    copy(b, e, begin);
+}
+
+
+template <typename T>
+void merge_sort(const T begin, const T end, bool(*compare)(T, T)){
+    size_t dist = distance(begin, end);
+
+    if (dist <= 1) return;
+    size_t mid = dist/2;
+
+    merge_sort(begin, begin+mid, compare);
+    merge_sort(begin+mid, end, compare);
+    
+    
+    merge(dist, begin, begin+mid, end, compare);
+}
+
+template<typename T>
+void parallel_merge_sort(const T begin, const T end, bool(*compare)(T, T)){
+    int num_threads = std::thread::hardware_concurrency();
+    int part_len = int(distance(begin, end)/num_threads);
+    std::vector<int> points(num_threads+1);
+    for (int i=0; i<num_threads;i++){
+        points.at(i) = i*part_len;
+    }
+    points.at(num_threads) = distance(begin, end);
+    std::vector<std::thread> threads(num_threads);
+    std::vector<std::packaged_task<void(T, T, bool(T, T))>> merge_sort_tasks(num_threads); 
+    std::vector<std::future<void>> results(num_threads);
+    int iter = 0;
+    for (auto& t: threads){
+        merge_sort_tasks.at(iter) = std::packaged_task<void(T, T, bool(T, T))>(merge_sort<T>);
+        results.at(iter) = merge_sort_tasks.at(iter).get_future();
+        t = std::thread(std::move(merge_sort_tasks.at(iter)), begin+points.at(iter), begin+points.at(iter+1), compare);
+        iter++;
+    }
+    for (auto& t : threads){
+        t.join();
+    }
+    std::vector<std::packaged_task<void(int,const T, T, const T, bool(T, T))>> merge_tasks(int(points.size()/2));
+    std::vector<std::future<void>> m_results(int(points.size()/2));
+    std::vector<int> tmp;
+    while(points.size() != 2){
+        int i = 0;
+        for (unsigned int j=0;j<points.size()-2; j+=2){
+            merge_tasks.at(i) = std::packaged_task<void(int,const T, T, const T, bool(T, T))>(merge<T>);
+            m_results.at(i) = merge_tasks.at(i).get_future();
+            threads.at(i) = std::thread(std::move(merge_tasks.at(i)), points.at(j+2)-points.at(j), begin+points.at(j),
+                begin+points.at(j+1), begin+points.at(j+2), compare);
+            tmp.push_back(points.at(j));    
+            i++;  
+        }
+        tmp.push_back(points.back());
+        for (int j=0; j< i; j++)
+            threads.at(j).join();
+        points.swap(tmp);
+        tmp.clear();    
+    }
+}
---
+++ Task08/src/project/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "MergeSort.h"
+#include <chrono>
+#include <algorithm>
+#include <cstdlib>
+
+
+
+int main(){
+    
+    for (unsigned long long size = 1; size < 1000000; size *= 10) {
+        std::vector<int> v1(size);
+        std::generate(v1.begin(), v1.end(), std::rand);
+        std::vector<int> v2(v1);
+        auto start = std::chrono::system_clock::now();
+        merge_sort(v1.begin(), v1.end(), &my_compare);
+        auto end = std::chrono::system_clock::now();
+        std::cout << size <<" " << (end-start).count()<<" ";
+        start = std::chrono::system_clock::now();
+        parallel_merge_sort(v2.begin(), v2.end(), &my_compare);
+        end = std::chrono::system_clock::now();
+        std::cout<<(end-start).count()<<std::endl;
+    }
+    return 0;
+} 
